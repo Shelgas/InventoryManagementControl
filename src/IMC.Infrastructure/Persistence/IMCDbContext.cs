ebac817eb832dd33bf20a3647884409c5a995b21
Shelgas <akozlov1997@gmail.com>
2022-06-10 14:37:02 +0300

    added data seeding

--- src/IMC.Infrastructure/Persistence/IMCDbContext.cs
+++ src/IMC.Infrastructure/Persistence/IMCDbContext.cs
@@ -22,7 +22,11 @@ namespace IMC.Infrastructure.Persistence
         {
             modelBuilder.ApplyConfiguration(new PaintConfiguration());
             modelBuilder.Entity<Paint>().HasData(
-                new Paint { });
+                new Paint { Id = Guid.NewGuid(), Title = "Warboss Green", Type = "Layer", Column = 1 , Row = 1, Color = "#63b521" },
+                new Paint { Id = Guid.NewGuid(), Title = "Nuln Oil", Type = "Shade", Column = 1, Row = 2, Color = "#000000" },
+                new Paint { Id = Guid.NewGuid(), Title = "Dryad Bark", Type = "Base", Column = 1, Row = 3, Color = "#33312D" },
+                new Paint { Id = Guid.NewGuid(), Title = "Screaming Skull", Type = "Layer", Column = 1, Row = 4, Color = "#f0f1ce" }
+                );
         }
 
     }
