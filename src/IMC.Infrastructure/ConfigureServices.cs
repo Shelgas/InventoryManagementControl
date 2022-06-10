bf800aca67210919e7ab5fc0619d19559eb6572d
Shelgas <akozlov1997@gmail.com>
2022-06-10 16:42:21 +0300

    added dependancy injection

---
+++ src/IMC.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace IMC.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
