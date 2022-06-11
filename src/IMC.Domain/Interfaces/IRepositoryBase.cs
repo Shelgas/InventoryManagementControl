3e0f32cdb9284ff5f0278279815d3e2c8fe3cbc5
Shelgas <akozlov1997@gmail.com>
2022-06-11 22:30:21 +0300

    added repository pattern

---
+++ src/IMC.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC.Domain.Interfaces
+{
+    internal interface IRepositoryBase<T>
+    {
+        Task<IQueryable<T>> FindAllAsync();
+        Task<IQueryable<T>> FindByConditionAsync(Expression<Func<Task, bool>> expression);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
