17b910b06f633bf462cfe5a6d09daa14a03e3bb4
Shelgas <akozlov1997@gmail.com>
2022-06-12 20:21:41 +0300

    added  CQRS pattern for Get all Paint request

---
+++ src/IMC.Application/Paints/Queries/GetPaintList/GetPaintListQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using IMC.Domain;
+
+namespace IMC.Application.Paints.Queries.GetPaintList
+{
+    public class GetPaintListQuery : IRequest<IList<Paint>>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public string? Type { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ src/IMC.Application/Paints/Queries/GetPaintList/GetPaintListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using IMC.Domain;
+using Microsoft.EntityFrameworkCore;
+using IMC.Domain.Interfaces;
+
+namespace IMC.Application.Paints.Queries.GetPaintList
+{
+    public class GetPaintListQueryHandler : IRequestHandler<GetPaintListQuery, IEnumerable<Paint>>
+    {
+        private readonly IRepositoryWrapper _repository;
+
+        public GetPaintListQueryHandler(IRepositoryWrapper repository)
+        {
+            _repository = repository;
+        }
+
+
+        public async Task<IEnumerable<Paint>> Handle(GetPaintListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var paintQuery = await _repository.Piant.GetAllPaintAsync();
+            return paintQuery;
+        }
+    }
+}
